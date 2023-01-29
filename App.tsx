679d4b720852aa4cade4a30600fb29fd58497155
Daniel Ferraz <daniel0ferraz@outlook.com>
2023-01-28 23:32:50 -0300

    feat: adicionado animação ao logar

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {ThemeProvider} from 'styled-components';
 import {Routes} from './src/routes';
 import theme from './src/styles/theme';
+import 'react-native-reanimated';
 
 const App = () => {
   return (
